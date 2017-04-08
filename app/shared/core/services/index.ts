4f28ef1b20adbe04b4ddaff03287ac224ed095a1
vakrilov <alexander.vakrilov@telerik.com>
2017-04-08 19:28:52 +0300

    Chpater 1 - completed

---
+++ app/shared/core/services/index.ts
@@ -0,0 +1,10 @@
+import { DatabaseService } from './database.service';
+import { LogService } from './log.service';
+
+export const PROVIDERS: any[] = [
+    DatabaseService,
+    LogService
+];
+
+export * from './database.service';
+export * from './log.service';
---
+++ app/shared/core/services/log.service.ts
@@ -0,0 +1,5 @@
+// angular
+import { Injectable } from '@angular/core';
+@Injectable()
+export class LogService {
+}
