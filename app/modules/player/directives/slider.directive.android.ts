74cedcbe5ff77d65207b190ec96853bf2695b7ba
vakrilov <alexander.vakrilov@telerik.com>
2017-05-11 00:43:44 +0300

    Chapter 7: Slim slider directive

---
+++ app/modules/player/directives/slider.directive.android.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef } from '@angular/core';
+@Directive({
+    selector: '[slim-slider]'
+})
+export class SlimSliderDirective {
+    constructor(private el: ElementRef) { }
+    ngOnInit() {
+        let seekBar = <android.widget.SeekBar>this.el
+            .nativeElement.android;
+        seekBar.setOnTouchListener(
+            new android.view.View.OnTouchListener({
+                onTouch(view, event) {
+                    return true;
+                }
+            })
+        );
+        seekBar.getThumb().mutate().setAlpha(0);
+    }
+}
--- app/modules/player/player.module.ts
+++ app/modules/player/player.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 // app
 import { SharedModule } from '../shared/shared.module';
 import { COMPONENTS } from './components';
+import { DIRECTIVES } from './directives';
 import { PROVIDERS } from './services';
+
 @NgModule({
     imports: [ SharedModule ],
     providers: [ ...PROVIDERS ],
-    declarations: [ ...COMPONENTS ],
+    declarations: [ ...COMPONENTS, ... DIRECTIVES],
     exports: [
         SharedModule,
         ...COMPONENTS
